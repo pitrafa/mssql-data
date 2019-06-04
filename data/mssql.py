d505ced9d582b9a1b57f88300d4f5cd4c988a230
Felix De Gracia <pitrafa@gmail.com>
2019-06-04 09:59:15 -0400

    Initial commit

---
+++ data/mssql.py
@@ -0,0 +1,49 @@
+try:
+    import pyodbc
+except Exception as error:
+    print(error)
+
+
+class MsSQL(object):
+
+    def __init__(self, settings):
+        self.c = self.connect(settings)
+
+    def connect(self, credentials):
+        try:
+            conn = pyodbc.connect(
+                credentials,
+                autocommit=True
+            )
+        except Exception as error:
+            raise error
+
+        return conn
+
+    def query_object(self, sqlcommand):
+        cursor = self.c.cursor()
+        cursor.execute(sqlcommand)
+        result = self.map_keys(cursor)
+        if result:
+            result = result[0]
+        return result
+
+    def execute_query(self, sqlcommand):
+        cursor = self.c.cursor()
+        cursor.execute(sqlcommand)
+        result = self.map_keys(cursor)
+        return result
+
+    def execute_non_query(self, sqlcommand):
+        cursor = self.c.cursor()
+        cursor.execute(sqlcommand)
+
+    def map_keys(self, cursor):
+        result = [
+            dict(zip([column[0] for column in cursor.description], row))
+            for row in cursor.fetchall()
+        ]
+        return result
+
+    def __del__(self):
+        self.c.close()
